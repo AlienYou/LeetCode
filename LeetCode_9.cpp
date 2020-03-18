5127cd47337f68b0f345fdec6e6e0a616e269c52
Alien <AlienYou1997@163.com>
2020-03-17 21:06:39 -0700

    Palindrome Number

--- LeetCode_9.cpp
+++ LeetCode_9.cpp
@@ -17,6 +17,19 @@ long reverse(int x)
     }
     return reInterger;
 }
+
+bool isPalindrome_2(int x) {
+   if (x < 0 || (x % 10 == 0 && x != 0))
+        return false; 
+    int reverHalf = 0;
+    while (x > reverHalf)
+    {
+        reverHalf = reverHalf * 10 + x % 10;
+        x /= 10;
+    }
+    return x == reverHalf || (reverHalf / 10 == x) ;
+}
+
 bool isPalindrome(int x) {
    if (x < 0)
         return false; 
