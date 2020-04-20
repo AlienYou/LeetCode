b1b983deab6988ab573914f309d637d0eb05ebe7
Alien <AlienYou1997@163.com>
2020-04-20 00:27:15 -0700

    Numbers of Islands

--- LeetCode_200.cpp
+++ LeetCode_200.cpp
@@ -53,3 +53,74 @@ int numIslands(vector<vector<char>>& grid) {
     }
     return result;
 }
+
+class UnionFind
+{
+    public:
+    UnionFind(vector<vector<char>>& grid)
+    {
+        count = 0;
+        for (int i = 0; i < grid.size(); ++i)
+        {
+            for (int j = 0; j < grid[0].size(); ++j)
+            {
+                if (grid[i][j] == '1')
+                {  
+                    parent.push_back(i * grid[0].size() + j);
+                    count++;
+                }
+                else
+                    parent.push_back(-1);
+            }
+        }
+    }
+    int find(int i)
+    {
+        if (parent[i] != i)
+            parent[i] = find(parent[i]);
+        return parent[i];
+    }
+
+    void unite(int x, int y)
+    {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY)
+        {
+            parent[rootX] = rootY;
+            --count;
+        }
+    }
+
+    int getCount() const
+    {
+        return count;
+    }
+
+    private:
+        vector<int> parent;
+        int count;
+};
+int numIslands1(vector<vector<char>>& grid) {
+        if (grid.size() == 0)
+            return 0;
+        UnionFind unF(grid);
+        int n = grid.size();
+        int m = grid[0].size();
+        int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (grid[i][j] == '1')
+                {
+                    grid[i][j] = '0';
+                    if (i - 1 >= 0 && grid[i - 1][j] == '1') unF.unite(i * m + j, (i - 1) * m + j);
+                    if (i + 1 < n && grid[i + 1][j] == '1') unF.unite(i * m + j, (i + 1) * m + j);
+                    if (j - 1 >= 0 && grid[i][j - 1] == '1') unF.unite(i * m + j, i * m + j - 1);
+                    if (j + 1 < m && grid[i][j + 1] == '1') unF.unite(i * m + j, i * m + j + 1);
+                }
+            }
+        }
+        return unF.getCount();
+    }
