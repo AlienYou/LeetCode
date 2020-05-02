5d74486b2ffbbb19ceade9cd392d2dc9ffd8eda7
Alien <AlienYou1997@163.com>
2020-05-02 02:56:09 -0700

    Longest Substring Without Repeating Characters

--- LeetCode_3.cpp
+++ LeetCode_3.cpp
@@ -33,3 +33,21 @@ int lengthOfLongestSubstring(string s) {
     }
     return res;
 }
+
+int lengthOfLongestSubstring2(string s)
+{
+int length = s.length();
+    int res = 0;
+    int hash[128];
+    memset(hash, 0, sizeof(hash));
+    for (int i = 0, j = 0; j < length; ++j)
+    {
+        hash[s[j]]++;
+        while (hash[s[j]] > 1)
+        {
+            hash[s[i++]]--;
+        }
+        res = max(res, j - i + 1);
+    }
+    return res;
+}
