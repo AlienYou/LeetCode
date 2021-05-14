cbb09cb0e6814d5f150b0be0ed0b0d9b955958ea
AlienYou <AlienYou1997@163.com>
2021-05-14 17:58:35 +0800

    更新816

---
+++ 816.ambiguous-coordinates.cpp
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=816 lang=cpp
+ *
+ * [816] Ambiguous Coordinates
+ *
+ * https://leetcode.com/problems/ambiguous-coordinates/description/
+ *
+ * algorithms
+ * Medium (47.87%)
+ * Likes:    207
+ * Dislikes: 510
+ * Total Accepted:    22.6K
+ * Total Submissions: 40.8K
+ * Testcase Example:  '"(123)"'
+ *
+ * We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)".  Then,
+ * we removed all commas, decimal points, and spaces, and ended up with the
+ * string s.  Return a list of strings representing all possibilities for what
+ * our original coordinates could have been.
+ * 
+ * Our original representation never had extraneous zeroes, so we never started
+ * with numbers like "00", "0.0", "0.00", "1.0", "001", "00.01", or any other
+ * number that can be represented with less digits.  Also, a decimal point
+ * within a number never occurs without at least one digit occuring before it,
+ * so we never started with numbers like ".1".
+ * 
+ * The final answer list can be returned in any order.  Also note that all
+ * coordinates in the final answer have exactly one space between them
+ * (occurring after the comma.)
+ * 
+ * 
+ * Example 1:
+ * Input: s = "(123)"
+ * Output: ["(1, 23)", "(12, 3)", "(1.2, 3)", "(1, 2.3)"]
+ * 
+ * 
+ * 
+ * Example 2:
+ * Input: s = "(00011)"
+ * Output:  ["(0.001, 1)", "(0, 0.011)"]
+ * Explanation: 
+ * 0.0, 00, 0001 or 00.01 are not allowed.
+ * 
+ * 
+ * 
+ * Example 3:
+ * Input: s = "(0123)"
+ * Output: ["(0, 123)", "(0, 12.3)", "(0, 1.23)", "(0.1, 23)", "(0.1, 2.3)",
+ * "(0.12, 3)"]
+ * 
+ * 
+ * 
+ * Example 4:
+ * Input: s = "(100)"
+ * Output: [(10, 0)]
+ * Explanation: 
+ * 1.0 is not allowed.
+ * 
+ * 
+ * 
+ * 
+ * Note: 
+ * 
+ * 
+ * 4 <= s.length <= 12.
+ * s[0] = "(", s[s.length - 1] = ")", and the other elements in s are
+ * digits.
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> ambiguousCoordinates(string s) {
+        int n = s.length();
+        vector<string> ans;
+        for (int i = 1; i < n - 2; ++i) {
+            vector<string> left = f(s, 1, i);
+            vector<string> right = f(s, i + 1, n - 2 - i);
+            for (auto& str1 : left) {
+                for (auto& str2 : right) {
+                    ans.emplace_back("(" + str1 + ", " + str2 + ")");
+                }
+            } 
+        }
+        return ans;
+    }
+    
+    vector<string> f(string& s, int i, int j) {
+        string str = s.substr(i, j);
+        int n = str.length();
+        if (n == 0 || (n > 1 && str[0] == '0' && str[n - 1] == '0')) return {};
+        if (n > 1 && str[0] == '0') return {"0." + str.substr(1)};
+        if (n == 1 || str[n - 1] == '0') return {str};
+        vector<string> res = {str};
+        for (int i = 1; i < n; ++i) {
+            res.emplace_back(str.substr(0, i) + "." + str.substr(i));
+        }
+        return res;
+    }
+};
+// @lc code=end
+
